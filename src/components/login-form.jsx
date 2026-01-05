a7896ab620bc1f78433f8512e646008e4ed53dc4
AryaMerlet <151627770+AryaMerlet@users.noreply.github.com>
2026-01-05 16:46:25 +0100

    Remove signup link from login form and update .gitignore
    Deleted the 'Don't have an account? Sign up' link from the login form component. Also added .env to .gitignore to prevent environment files from being tracked.

--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -74,9 +74,6 @@ export default function LoginForm({ className, ...props }) {
 							</Field>
 							<Field>
 								<Button type="submit">Login</Button>
-								<FieldDescription className="text-center">
-									Don&apos;t have an account? <a href="/signup">Sign up</a>
-								</FieldDescription>
 							</Field>
 						</FieldGroup>
 					</form>
