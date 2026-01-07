275db5306c2975a8600556d62df15e3d8e5c25d5
AryaMerlet <151627770+AryaMerlet@users.noreply.github.com>
2026-01-07 16:51:33 +0100

    Update user state to include profile in AuthContext
    Modified the AuthContext to set the user state with both the user ID and the user's profile after sign-in. This change ensures that user profile information is readily available in the context.

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -37,7 +37,11 @@ export const AuthContext = ({ children }) => {
 		if (error) {
 			console.error("Error signing in:", error);
 		} else {
-			setUser(data);
+			const userProfile = {
+				...data.user.id,
+				profile: await getUserProfile(data.user.id),
+			};
+			setUser(userProfile);
 		}
 	}
 
